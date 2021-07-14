78814aefe9762d73d4b7747be355401edac26528
elokaagu <eloka.agu@icloud.com>
2021-07-14 20:12:58 +0100

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Eloka Agu on 14/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
